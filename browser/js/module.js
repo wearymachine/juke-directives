8be2ef2adbaccf19e7e58c00753467b7a54fc18b
Sean Sarkar <wearymachine@Seans-MacBook-Air.local>
2015-12-08 15:57:11 -0500

    got stuff working

--- browser/js/module.js
+++ browser/js/module.js
@@ -1,1 +1,8 @@
 var app = angular.module('juke', ['ui.router', 'ngMessages']);
+
+// app.directive('sidebar',function(){
+// 	return {
+// 		restrict: 'E',
+// 		templateUrl: '../templates/sidebarDirective.html'
+// 	}
+// })
